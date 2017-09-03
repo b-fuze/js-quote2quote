e4ae29446993ef70ea2d046424f540c42881d8cf
Jean-Philippe Sirois <jp@veksen.net>
2017-09-03 19:25:58 -0400

    test the example from the readme

---
+++ test-out.js
@@ -0,0 +1,11 @@
+// Don't read these comments
+//
+// I'm going through too "much" effort for this lmao
+
+const thing = "Hi, I'm bobby, I \"like\" cats";
+const inconsistent = "So, I'm fired eh?\n \"No. You're under arrest.\"";
+const otherThing = `This template string has "things" to say, don't you think?`;
+
+/*
+  Stuff 'here' and "here" don't work man
+ */
--- test.js
+++ test.js
@@ -21,6 +21,15 @@ test('replace all double quotes to single quotes', () => {
   expect(switchStringLiterals(input, `'`)).toBe(output);
 });
 
+// not sure how to name this test
+// should prob be rewritten
+test('complete in/out from the example', () => {
+  const fs = require('fs');
+  const input  = fs.readFileSync('./test-in.js', {encoding: "utf8"});
+  const output = fs.readFileSync('./test-out.js', {encoding: "utf8"});
+  expect(switchStringLiterals(input, `"`)).toBe(output);
+});
+
 test('avoid replacing quotes inside // comments', () => {
   const input  = `// this is a comment with 'single' and "double" quotes`;
   const output = `// this is a comment with 'single' and "double" quotes`;
